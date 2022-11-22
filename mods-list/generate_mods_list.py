2f0d31ec12f91b2a17aaf8c0685c23ea06d9131e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-11-22 15:17:47 -0600

    [no ci] Improve list generator

--- mods-list/generate_mods_list.py
+++ mods-list/generate_mods_list.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 
 import tomli
 
@@ -12,32 +13,44 @@ ignored_mods = load_mod_set('mods-list/ignored.txt')
 library_mods = load_mod_set('mods-list/library.txt')
 optimization_mods = load_mod_set('mods-list/optimization-client.txt')
 
+with open('mods-list/extra_meta.toml', 'rb') as fp:
+    extra_meta = tomli.load(fp)
+
 content_mods_text = '## Content Mods\n\n'
 library_mods_text = '## Library Mods\n\n'
 optimization_mods_text = '## Optimization/Client Mods\n\n'
 
 for filename in sorted(os.listdir('mods')):
-    if not filename.endswith('.pw.toml'):
-        continue
-    mod_name = filename.removesuffix('.pw.toml')
+    mod_name = filename.removesuffix('.pw.toml').removesuffix('.jar')
     # print(f'Processing {mod_name}...', end=' ')
     if mod_name in ignored_mods:
         # print('Skipped')
         continue
-    with open(f'mods/{filename}', 'rb') as fp:
-        mod_data = tomli.load(fp)
+    url: str | None = None
+    if filename.endswith('.pw.toml'):
+        with open(f'mods/{filename}', 'rb') as fp:
+            mod_data = tomli.load(fp)
+        friendly_name: str = mod_data['name']
+        try:
+            update_origin: str = next(iter(mod_data['update']))
+            match update_origin:
+                case 'curseforge':
+                    url = f'https://www.curseforge.com/minecraft/mc-mods/{mod_name}'
+                case 'modrinth':
+                    url = f'https://modrinth.com/mod/{mod_data["update"]["modrinth"]["mod-id"]}'
+        except KeyError:
+            pass
+    else:
+        friendly_name = mod_name
+    if mod_name in extra_meta:
+        mod_extra_meta: dict[str, str] = extra_meta[mod_name]
+        friendly_name = mod_extra_meta['name']
+        url = mod_extra_meta['url']
     mod_text = '  + '
-    try:
-        update_origin: str = next(iter(mod_data['update']))
-        mod_text += f'[{mod_data["name"]}]('
-        match update_origin:
-            case 'curseforge':
-                mod_text += f'https://www.curseforge.com/minecraft/mc-mods/{mod_name}'
-            case 'modrinth':
-                mod_text += f'https://modrinth.com/mod/{mod_data["update"]["modrinth"]["mod-id"]}'
-        mod_text += ')'
-    except KeyError:
-        mod_text += mod_data['name']
+    if url is None:
+        mod_text += friendly_name
+    else:
+        mod_text += f'[{friendly_name}]({url})'
     mod_text += '\n'
     if mod_name in library_mods:
         library_mods_text += mod_text
